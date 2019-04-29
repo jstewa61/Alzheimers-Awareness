75446e8bbe83b13204e5dd0a6e4830700adefc0c
jstewa61 <jstewa61@kent.edu>
2019-04-29 00:53:21 -0400

    Added CSS Grid

--- script.js
+++ script.js
@@ -15,3 +15,27 @@ $(".container").click(function() {
 });
 
 $('.container').on('touchstart', function() {});
+
+/* Button */
+
+function hover() {
+  $(".button").on("mouseenter", function() {
+    return $(this).addClass("hover");
+  });
+}
+
+function hoverOff() {
+  $(".button").on("mouseleave", function() {
+    return $(this).removeClass("hover");
+  });
+}
+
+function active() {
+  $(".button").on("click", function() {
+    return $(this).addClass("active");
+  });
+}
+
+hover();
+hoverOff();
+active();
